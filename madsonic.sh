d4e45e677148cf9f83ef5569f9e2e352beed96ca
Troy Olson <troyolson1@gmail.com>
2014-06-26 22:17:42 -0700

    Update madsonic.sh

--- madsonic.sh
+++ madsonic.sh
@@ -18,6 +18,6 @@ export LC_ALL=en_US.UTF-8
 /usr/bin/madsonic --home=$HOME \
                   --host=$HOST \
                   --port=$PORT \
-                  --max-memory=$MAX_MEMORY \
+                  --max-memory=$MAX_MEMORY 
 sleep 5
 tail -f /config/madsonic_sh.log
